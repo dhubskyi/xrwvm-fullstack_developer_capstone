17c221e4e5c19e0a79a80aff3b9e40b57eb17263
dhubskyi <hubskyydenys@gmail.com>
2024-11-11 22:15:37 -0500

    Adding admin db functionality

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,16 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display=['CarModel', 'CarMake']
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+admin.site.register(CarModel)
