29aaf4584023490e31e1b9c05bdeea3c789af6a4
dhubskyi <hubskyydenys@gmail.com>
2024-11-15 14:22:22 +1100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='get_cars', view=views.get_cars, name='getcars'),
     # path for dealer reviews view
-    # path(route='get_dealers', view=views.get_dealerships, 
+    # path(route='get_dealers', view=views.get_dealerships,
     # name='get_dealers'),
     path(route='get_dealers/<str:state>',
          view=views.get_dealerships, name='get_dealers_by_state'),
