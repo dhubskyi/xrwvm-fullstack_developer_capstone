c549950e2c9e8daafa573bf5fd00849cf4a93cb3
dhubskyi <hubskyydenys@gmail.com>
2024-11-15 14:12:09 +1100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://localhost",
+ALLOWED_HOSTS = [
+    "https://localhost",
     "hubskyydenys-8000.theiadockernext-1-"+
         "labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
     "0.0.0.0"]
