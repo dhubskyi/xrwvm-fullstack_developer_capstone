dedb2c0c116211b70d84e1f5f79acd841f3ddc25
dhubskyi <hubskyydenys@gmail.com>
2024-11-15 13:35:00 +1100

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
             name='CarModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True,
-                    serialize=False, verbose_name='ID')),
+                serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
                 ('type', models.CharField(choices=[
                     ('SEDAN', 'Sedan'), ('SUV', 'SUV'),
