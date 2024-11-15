87c8c658f65e34cf48d4a1f76633be6ef0bf952d
dhubskyi <hubskyydenys@gmail.com>
2024-11-15 13:34:04 +1100

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -19,8 +19,7 @@ class Migration(migrations.Migration):
                 ('id',
                  models.BigAutoField(
                     auto_created=True, primary_key=True,
-                    serialize=False, verbose_name='ID')
-                ),
+                    serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
                 ('description', models.CharField(max_length=1023)),
             ],
@@ -29,20 +28,16 @@ class Migration(migrations.Migration):
             name='CarModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True,
-                    serialize=False, verbose_name='ID')
-                ),
+                    serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
                 ('type', models.CharField(choices=[
                     ('SEDAN', 'Sedan'), ('SUV', 'SUV'),
                     ('WAGON', 'Wagon')], default='SUV',
                     max_length=15)),
                 ('year', models.IntegerField(default=2024,
-                    validators = [
+                    validators=[
                         django.core.validators.MaxValueValidator(2024),
-                        django.core.validators.MinValueValidator(2015)
-                    ]
-                                            )
-                ),
+                        django.core.validators.MinValueValidator(2015)])),
                 ('car_make', models.ForeignKey(
                     on_delete=django.db.models.deletion.CASCADE,
                     to='djangoapp.carmake')),
